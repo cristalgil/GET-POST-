532db66d01b4eddde579bb5c07e89d937ca1cf38
cristalgil <137519536+cristalgil@users.noreply.github.com>
2023-06-28 12:31:00 -0600

    Add files via upload

--- datos.php
+++ datos.php
@@ -1,7 +1,18 @@
 <?php
-//$nombre = $_GET ['txtNombre'];
-$nombre = $_POST ['txtNombre'];
-//$apellidos = $_GET ['txtApellidos'];
-$apellidos =$_POST ['txtApellidos'];
-echo $nombre. "/" .$apellidos;
+$nombre = $_GET["nombre"];
+//$nombre = $_POST["nombre"];
+$email = $_GET["email"];
+//$email = $_POST["email"];
+$telefono = $_GET["telefono"];
+//$telefono = $_POST["telefono"];
+$sitio = $_GET["sitio"];
+//$sitio = $_POST["sitio"];
+$asunto = $_GET["asunto"];
+//$asunto = $_POST["asunto"];
+$mensaje = $_GET["mensaje"];
+//$mensaje = $_POST["mensaje"];
+
+
+echo $nombre." ". $email." ". $telefono."".$sitio." ". $asunto." ". $mensaje;
+
 ?>
